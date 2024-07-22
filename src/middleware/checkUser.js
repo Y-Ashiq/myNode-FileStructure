38bf9fdaba45146bcf48c63242e24fe5c1009ee2
Youssef Alashiq <100365853+Y-Ashiq@users.noreply.github.com>
2024-07-22 12:52:27 +0300

    Update checkUser.js

--- src/middleware/checkUser.js
+++ src/middleware/checkUser.js
@@ -1,15 +1,20 @@
 import userModel from "../database/models/user.model.js";
+import validator from "validator";
 import bcrypt from "bcrypt";
 
 export const checkUser = async (req, res, next) => {
   let { email } = req.body;
 
-  let isExist = await userModel.findOne({email});
-
-  if (isExist) {
-    res.json({ message: " this userName is already exist" });
+  if (!validator.isEmail(email)) {
+    res.status(422).json({ message: "invalid email" });
   } else {
-    req.body.password = bcrypt.hashSync(req.body.password, 8);
-    next();
+    let isExist = await userModel.findOne({ email });
+
+    if (isExist) {
+      res.json({ message: " this userName is already exist" });
+    } else {
+      req.body.password = bcrypt.hashSync(req.body.password, 8);
+      next();
+    }
   }
 };
